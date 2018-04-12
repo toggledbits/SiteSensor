2549f7954a57328fed13a329cbaf1382fd4831b1
Patrick Rigney <patrick@toggledbits.com>
2018-04-12 14:34:23 -0400

    Add contents of Value1 expression result to AltUI dashboard card

--- J_SiteSensor1_ALTUI.js
+++ J_SiteSensor1_ALTUI.js
@@ -7,13 +7,16 @@ var SiteSensor_ALTUI = ( function( window, undefined ) {
                 var html ="";
                 var message = MultiBox.getStatus( device, "urn:toggledbits-com:serviceId:SiteSensor1", "Message");
                 var st = MultiBox.getStatus( device, "urn:micasaverde-com:serviceId:SecuritySensor1", "Armed");
-                html += '<div class="pull-left">';
+                html += '<div style="font-size: 0.8em;">';
                 html += message;
                 html += "</div>";
                 html += ALTUI_PluginDisplays.createOnOffButton( st, "toggledbits-sitesensor-" + device.altuiid, _T("Disarmed,Armed"), "pull-right");
                 html += "<script type='text/javascript'>";
                 html += "$('div#toggledbits-sitesensor-{0}').on('click', function() { SiteSensor_ALTUI.toggleArmed('{0}','div#toggledbits-sitesensor-{0}'); } );".format(device.altuiid);
                 html += "</script>";
+                html += '<div>';
+                html += MultiBox.getStatus( device, "urn:toggledbits-com:serviceId:SiteSensor1", "Value1" )
+                html += '</div>';
                 return html;
         }
     return {
